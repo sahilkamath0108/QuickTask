864594e5054fefb52611730a1a0e6eb7e0ed0898
Sahil Kamath <sahilkamath0108@gmail.com>
2023-02-19 12:33:44 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+require("./db")
+require('dotenv').config()
+// const providerRoutes = require("./routes/providerRoutes")
+// const userRoutes = require("./routes/userRoutes")
+// const adminRoutes = require("./routes/adminRoutes")
+// const foodRoutes = require("./routes/foodRoutes")
+const cors = require("cors")
+const bodyParser = require("body-parser")
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json());
+
+
+
+app.use(express.json())
+
+// User 
+app.use('/user',userRoutes)
+
+//provider
+app.use('/provider',providerRoutes)
+
+//admin
+app.use('/admin',adminRoutes);
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+  console.log('The server is up and running at port 3000')
+})
+
+
---
+++ models/noteSchema.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require('dotenv').config();
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+    },
+    isCompleted: {
+        type: Boolean,
+        default: false
+    },
+}, {timestamps: true});
+
+
+const NoteSchema = mongoose.model("user", noteSchema);
+module.exports = NoteSchema;
